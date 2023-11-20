bb9384466430b521c9cb819819e3dc340aa734bc
Esa Kesti <esakesti@gmail.com>
2023-11-20 22:50:57 +0000

    update

---
+++ Dictionary/hippo.py
@@ -0,0 +1,14 @@
+animal = "Lamborghini"
+
+print(animal[3:-5])
+print(animal[-4:])
+print(animal[:7])
+
+
+colors = ["red", "white", "blue"]
+colors.insert(2, "yellow")
+
+print(colors)
+
+teacher_names = {"math":"aniyah Cook", "Science":"Ines blisset", "Engineering":"Wayne Branon"}
+teacher_names.values()
