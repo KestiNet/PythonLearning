8832780b940566bdb9ff21b3199aefc0185d27e6
Esa Kesti <esakesti@gmail.com>
2024-01-21 21:29:35 +0000

    empty string error handling

--- Python OS/module_5(Testing)/rearrange_test.py
+++ Python OS/module_5(Testing)/rearrange_test.py
@@ -6,5 +6,13 @@ import unittest
 class TestRearange(unittest.TestCase):
     def test_basic(self):
         testcase = "Lovelace, Ada"
-        excepted = "Ada Lovelace"
-        self.assertEqual(rearrange_name(testcase), excepted)
+        expected = "Ada Lovelace"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_empty(self):
+        testcase = ""
+        expected = ""
+        self.assertEqual(rearrange_name(testcase), expected)
+
+
+unittest.main()
