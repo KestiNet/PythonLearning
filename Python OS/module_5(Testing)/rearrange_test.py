227140c3eeb6c2b7652cc73d67b0c586bec82ff2
Esa Kesti <esakesti@gmail.com>
2024-01-21 18:01:20 +0000

    testcase testing

--- Python OS/module_5(Testing)/rearrange_test.py
+++ Python OS/module_5(Testing)/rearrange_test.py
@@ -4,4 +4,7 @@ from reaarange import rearrange_name
 import unittest
 
 class TestRearange(unittest.TestCase):
-    
+    def test_basic(self):
+        testcase = "Lovelace, Ada"
+        excepted = "Ada Lovelace"
+        self.assertEqual(rearrange_name(testcase), excepted)
